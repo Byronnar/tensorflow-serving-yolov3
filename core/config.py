92365a92a49de42e0dbc9b8138032997f73254c8
Byronnar <48123414+Byronnar@users.noreply.github.com>
2019-10-16 17:02:58 +0800

    Update config.py

--- core/config.py
+++ core/config.py
@@ -27,7 +27,7 @@ __C.YOLO.DEMO_WEIGHT            = "./checkpoint/yolov3_coco_demo.ckpt"
 # Train options
 __C.TRAIN                       = edict()
 
-__C.TRAIN.ANNOT_PATH            = "./data/dataset/2007_train.txt"
+__C.TRAIN.ANNOT_PATH            = "./2007_train.txt"
 __C.TRAIN.BATCH_SIZE            = 6
 __C.TRAIN.INPUT_SIZE            = [320, 352, 384, 416, 448, 480, 512, 544, 576, 608]
 __C.TRAIN.DATA_AUG              = True
@@ -43,7 +43,7 @@ __C.TRAIN.INITIAL_WEIGHT        = "./checkpoint/yolov3_coco_demo.ckpt"
 # TEST options
 __C.TEST                        = edict()
 
-__C.TEST.ANNOT_PATH             = "./data/dataset/2007_test.txt"
+__C.TEST.ANNOT_PATH             = "./2007_test.txt"
 __C.TEST.BATCH_SIZE             = 2
 __C.TEST.INPUT_SIZE             = 544
 __C.TEST.DATA_AUG               = False
