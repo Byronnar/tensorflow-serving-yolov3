4be3cd3a2e55cd35e6b229fac06b2aaa83170e39
Byronnar <48123414+Byronnar@users.noreply.github.com>
2020-01-13 16:24:44 +0800

    update split.py

--- split.py
+++ split.py
@@ -5,70 +5,69 @@ import xml.etree.ElementTree as ET
 import random
 import os
 
+def base_txt():
+    saveBasePath = r"./VOC2007/ImageSets"              # txt文件保存目录
+    total_xml = os.listdir(r'./VOC2007/Annotations')   # 获取标注文件（file_name.xml）
 
-saveBasePath = r"./VOC2007/ImageSets"              # txt文件保存目录
-total_xml = os.listdir(r'./VOC2007/Annotations')   # 获取标注文件（file_name.xml）
+    # 划分数据集为（训练，验证，测试集 = 49%,20%,30%）
 
-# 划分数据集为（训练，验证，测试集 = 49%,20%,30%）
+    val_percent = 0.2                              # 可以自己修改
+    test_percent = 0.2
+    trainval_percent = 0.8
 
-val_percent = 0.1                              # 传参
-test_percent = 0.05
-trainval_percent = 0.95
+    # print(trainval_percent)
 
-# print(trainval_percent)
+    tv = int(len(total_xml) * trainval_percent)
+    #tr = int(len(total_xml) * train_percent)
+    ta = int(tv * val_percent)
+    tr = int(tv -ta)
+    tt = int(len(total_xml) * test_percent)
 
-tv = int(len(total_xml) * trainval_percent)
-#tr = int(len(total_xml) * train_percent)
-ta = int(tv * val_percent)
-tr = int(tv -ta)
-tt = int(len(total_xml) * test_percent)
+    # 打乱训练文件（洗牌）
+    trainval = random.sample(range(len(total_xml)), tv)
+    train = random.sample(trainval, tr)
 
-# 打乱训练文件（洗牌）
-trainval = random.sample(range(len(total_xml)), tv)
-train = random.sample(trainval, tr)
+    print("训练集图片数量：", tr)
+    print("验证集图片数量：", ta)
+    print("测试集图片数量：", tt)
 
-print("训练集图片数量：", tr)
-print("验证集图片数量：", ta)
-print("测试集图片数量：", tt)
+    # with open('/tmp/VOC2007/split.txt', 'w', encoding='utf-8') as f:
+    #     f.write(str(val_percent))
 
-# with open('/tmp/VOC2007/split.txt', 'w', encoding='utf-8') as f:
-#     f.write(str(val_percent))
+    ftrainval = open(os.path.join(saveBasePath, 'Main/trainval.txt'), 'w')
+    ftest = open(os.path.join(saveBasePath, 'Main/test.txt'), 'w')
+    ftrain = open(os.path.join(saveBasePath, 'Main/train.txt'), 'w')
+    fval = open(os.path.join(saveBasePath, 'Main/val.txt'), 'w')
 
-ftrainval = open(os.path.join(saveBasePath, 'Main/trainval.txt'), 'w')
-ftest = open(os.path.join(saveBasePath, 'Main/test.txt'), 'w')
-ftrain = open(os.path.join(saveBasePath, 'Main/train.txt'), 'w')
-fval = open(os.path.join(saveBasePath, 'Main/val.txt'), 'w')
-
-for i in range(len(total_xml)):                # 遍历所有 file_name.xml 文件
-    name = total_xml[i][:-4] + '\n'            # 获取 file_name
-    if i in trainval:
-        ftrainval.write(name)
-        if i in train:
-            ftrain.write(name)
+    for i in range(len(total_xml)):                # 遍历所有 file_name.xml 文件
+        name = total_xml[i][:-4] + '\n'            # 获取 file_name
+        if i in trainval:
+            ftrainval.write(name)
+            if i in train:
+                ftrain.write(name)
+            else:
+                fval.write(name)
         else:
-            fval.write(name)
-    else:
-        ftest.write(name)
+            ftest.write(name)
 
-ftrainval.close()
-ftrain.close()
-fval.close()
-ftest.close()
+    ftrainval.close()
+    ftrain.close()
+    fval.close()
+    ftest.close()
 
-sets=[ ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+base_txt()
 
-fi=open('./data/classes/vis.names','r')
-txt=fi.readlines()
-voc_class=[]
+fi = open('./data/classes/voc.names', 'r')      # 按文件夹里面的文件修改好
+txt = fi.readlines()
+voc_class = []
 for w in txt:
-    w=w.replace('\n','')
+    w = w.replace('\n', '')
     voc_class.append(w)
-print('数据集里面的类别：',voc_class)
-
+print('数据集里面的类别：', voc_class)
 classes = voc_class
 
-
 def convert_annotation(year, image_id, list_file):
+
     in_file = open('./VOC%s/Annotations/%s.xml'%(year, image_id))
     tree=ET.parse(in_file)
     root = tree.getroot()
@@ -84,6 +83,8 @@ def convert_annotation(year, image_id, list_file):
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
 wd = '.'
+
+sets=[ ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
 # wd = getcwd()
 for year, image_set in sets:
